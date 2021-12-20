edb49276a96c4884cfca20fe944ad8ad3b2fd1bd
xAHRx <alanhoskingrivera@gmail.com>
2021-12-20 00:27:01 -0400

    Added BooksController.cs

---
+++ My-Books/Controllers/BooksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace My_Books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+    }
+}
